a8e645af8d3779d65b7367ff21d78a52232ef6cc
mohammedabdelaleem <mohammedabdelaleem20@gmail.com>
2023-09-09 18:38:31 +0300

    After Adding 1st Extension:Add Date and Logged In User to All Screens.

--- OOP-Projects/6-BankSystem/clsScreen.h
+++ OOP-Projects/6-BankSystem/clsScreen.h
@@ -1,12 +1,14 @@
 #pragma once
 #include<iostream>
 #include"Global.h"
+#include"clsDate.h"
+
 
 using namespace std;
 
 class clsScreen
 {
-
+   
 protected:
 
 	static void _DrawScreenHeader(string Title, string SubTitle = "")
@@ -18,6 +20,12 @@ protected:
             cout << "\n\t\t\t\t\t  " << SubTitle;
         }
         cout << "\n\t\t\t\t\t______________________________________\n\n";
+
+        //01 Extention => Add Date and Logged In User to All Screens ==> The Power Of OOP And Inheritance.
+        cout<<setw(40)<<left<<"" << "User : " << CurrentUser.UserName << "\n";
+      // cout <<setw(40)<<left<<"" << "Date : " << clsDate::DateToString(clsDate::GetSystemDate()) <<"\n"<< endl;
+       cout << setw(40) << left << "" << "Date : " << clsDate::DateToString(clsDate()) << "\n" << endl;
+
     }
 
     static void moveCursor(int row, int col) {/////////////
